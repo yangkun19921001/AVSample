b3d5c36d373ee7fca87c875545d0e623a97c10ab
DevYK <yang1001yk@gmail.com>
2020-06-14 00:51:02 +0800

    mediacodec audio encode/decode  aac/pcm  completed

---
+++ mediacodec_audio_encode_decode/src/main/java/com/devyk/mediacodec_audio_encode/mediacodec/BaseCodec.kt
@@ -0,0 +1,14 @@
+package com.devyk.mediacodec_audio_encode.mediacodec
+
+import com.devyk.mediacodec_audio_encode.AudioConfiguration
+
+/**
+ * <pre>
+ *     author  : devyk on 2020-06-13 23:53
+ *     blog    : https://juejin.im/user/578259398ac2470061f3a3fb/posts
+ *     github  : https://github.com/yangkun19921001
+ *     mailbox : yang1001yk@gmail.com
+ *     desc    : This is BaseCoder
+ * </pre>
+ */
+class BaseCoder(private val mAudioConfiguration: AudioConfiguration?) {}
